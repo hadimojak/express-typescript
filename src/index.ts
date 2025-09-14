d3d1d95053e6bba70a808919280f9dc3c5946b12
h.arbabi <hadimojak@gmail.com>
2025-09-14 15:03:59 +0330

    loginRoutes

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(`
+        <div>
+        <h1>hi there</h1>
+        </div>`);
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
